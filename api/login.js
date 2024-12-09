f7bb7bed811899de99b1e4360162734ec4fd2208
Yi <125733585+zygame1314@users.noreply.github.com>
2024-12-10 03:41:09 +0800

    Add files via upload

--- api/login.js
+++ api/login.js
@@ -1,6 +1,10 @@
 import { verify } from './utils/auth.js';
 
 export default async function handler(req, res) {
+    if (req.method === 'OPTIONS') {
+        return res.status(200).send('OK');
+    }
+
     if (req.method !== 'POST') {
         return res.status(405).json({ message: '方法不允许' });
     }
