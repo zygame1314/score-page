7034fc071ee321502f98f03ff064ca4353c56ba8
Yi <125733585+zygame1314@users.noreply.github.com>
2024-12-10 04:20:54 +0800

    Add files via upload

--- api/login.js
+++ api/login.js
@@ -4,6 +4,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
 
     if (req.method === 'OPTIONS') {
         res.status(200).json({ status: 'ok' });
