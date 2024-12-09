92d73697588308d8f67268c314993b2e176b9738
Yi <125733585+zygame1314@users.noreply.github.com>
2024-12-09 23:07:17 +0800

    Add files via upload

--- api/utils/auth.js
+++ api/utils/auth.js
@@ -1,6 +1,6 @@
-import { UPYun } from 'upyun';
+const UPYUN = require('upyun');
 
-const upyun = new UPYun({
+const upyun = new UPYUN.Service({
     serviceName: process.env.UPYUN_SERVICE_NAME,
     operatorName: process.env.UPYUN_OPERATOR_NAME,
     password: process.env.UPYUN_OPERATOR_PASSWORD
@@ -8,10 +8,12 @@ const upyun = new UPYun({
 
 export async function verify(username, password) {
     try {
-        const users = await upyun.get('/users.json');
-        const userList = JSON.parse(users);
+        const client = new UPYUN.Client(upyun);
 
-        const user = userList.find(u =>
+        const result = await client.getFile('/users.json');
+        const users = JSON.parse(result.toString());
+
+        const user = users.find(u =>
             u.username === username && u.password === password
         );
 
