e6ecc75820cca2b2295a103a8352ebc6cb6be626
Yi <125733585+zygame1314@users.noreply.github.com>
2024-12-10 18:57:32 +0800

    Add files via upload

--- api/papers.js
+++ api/papers.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
 
             if (req.query.id) {
                 const filePath = '/papers/' + req.query.id;
-                const fileUrl = client.getSignedUrlSync(filePath);
+                const fileUrl = service.getSignatureUrl(filePath);
 
                 res.status(200).json({
                     id: req.query.id,
